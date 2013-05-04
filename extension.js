acb8ca43e32baceef00df1dbdea8b7d7ff911ecf
rajeshksv <rajeshksv37@gmail.com>
2013-05-04 16:26:24 +0530

    Code Indentation

--- extension.js
+++ extension.js
@@ -50,56 +50,56 @@ Soup.Session.prototype.add_feature.call(_httpSession, new Soup.ProxyResolverDefa
 
 
 function cricketScoreButton() {
-    this._init();
+	this._init();
 }
 
 
 cricketScoreButton.prototype = {
-  __proto__: PanelMenu.Button.prototype,
+	__proto__: PanelMenu.Button.prototype,
 
-  _init: function() {
-    PanelMenu.Button.prototype._init.call(this, 0.25);
+	_init: function() {
+		PanelMenu.Button.prototype._init.call(this, 0.25);
 
-    let topBox = new St.BoxLayout();
-    this._scoreInfo = new St.Label({ text: _('Loading.....') });
-    topBox.add_actor(this._scoreInfo);
-    this.actor.add_actor(topBox);
+		let topBox = new St.BoxLayout();
+		this._scoreInfo = new St.Label({ text: _('Loading.....') });
+		topBox.add_actor(this._scoreInfo);
+		this.actor.add_actor(topBox);
 
 
-    let children = null;
-    children = Main.panel._centerBox.get_children();
-    Main.panel._centerBox.insert_child_at_index(this.actor, children.length);
-		             
-    if(typeof Main.panel._menus == "undefined")
-	Main.panel.menuManager.addMenu(this.menu);
-    else
-	Main.panel._menus.addMenu(this.menu);
-       
+		let children = null;
+		children = Main.panel._centerBox.get_children();
+		Main.panel._centerBox.insert_child_at_index(this.actor, children.length);
+				     
+		if(typeof Main.panel._menus == "undefined")
+			Main.panel.menuManager.addMenu(this.menu);
+		else
+			Main.panel._menus.addMenu(this.menu);
 
-   this._moreInfo = new St.Bin();
-   this._refreshedDate = new St.Bin();
-   
-   this.menu.addActor(this._moreInfo);
-   let item = new PopupMenu.PopupSeparatorMenuItem();
-   this.menu.addMenuItem(item);
-   this.menu.addActor(this._refreshedDate);
 
-   this.showLoadingUi();
+		this._moreInfo = new St.Bin();
+		this._refreshedDate = new St.Bin();
 
-  Mainloop.timeout_add_seconds(10, Lang.bind(this, function() {
-      this.refreshScore(true);
-  }));
+		this.menu.addActor(this._moreInfo);
+		let item = new PopupMenu.PopupSeparatorMenuItem();
+		this.menu.addMenuItem(item);
+		this.menu.addActor(this._refreshedDate);
 
-  },
+		this.showLoadingUi();
 
-    destroyCurrentInfo: function() {
-        if (this._moreInfo.get_child() != null) {
-            this._moreInfo.get_child().destroy();
-         }   
-        if(this._refreshedDate.get_child() != null) {
-           this._refreshedDate.get_child().destroy();
-          }
-    },
+		Mainloop.timeout_add_seconds(10, Lang.bind(this, function() {
+		this.refreshScore(true);
+		}));
+
+	},
+
+	destroyCurrentInfo: function() {
+		if (this._moreInfo.get_child() != null) {
+		this._moreInfo.get_child().destroy();
+		}   
+		if(this._refreshedDate.get_child() != null) {
+		this._refreshedDate.get_child().destroy();
+		}
+	},
 
 
         load_xml_async: function(url, fun) {
@@ -110,80 +110,78 @@ cricketScoreButton.prototype = {
                 global.log("curl failed");
                return;
              }
-            fun.call(here,message.response_body.data) ;
-        });
-    },
+            fun.call(here,message.response_body.data) ;});
+    	},
+	
+	refreshScore: function(recurse) {
+        	url = "http://synd.cricbuzz.com/j2me/1.0/livematches.xml";
+        	this.load_xml_async(url , function(content) {
+            	try {
+              		// ECMA Script for XML (E4X)
+             		let oxml  = new XML(content);
+              		i=0;
+              		while(typeof(oxml.match[i]) != 'undefined'){
+				var d  = new Date();
+				var hrs = (d.getHours() < 10) ? ("0" + d.getHours()) : d.getHours();
+				var min = (d.getMinutes() < 10) ? ("0" + d.getMinutes()) : d.getMinutes();
+				var sec = (d.getSeconds() < 10) ? ("0" + d.getSeconds()) : d.getSeconds();
+				var currentTime = hrs + " : " + min + " : " + sec + " IST ";
+				if(oxml.match[i].state.@mchState == "preview") {
+					  let team1 = oxml.match[i].Tm[0].@sName.toString();
+					  let team2 = oxml.match[i].Tm[1].@sName.toString();
+					  let status = oxml.match[i].state.@status;
+
+					  this._scoreInfo.text = team1 + " vs " + team2 + " ( Upcoming ) ";
+					  this._moreInfo.get_child().text = "Dont waste your time ! Match " + status; 
+					  this._refreshedDate.get_child().text = "  Last Refreshed on   " + currentTime; 
+				} else { 
+					let team1 = oxml.match[i].mscr.btTm.@sName.toString();
+					let team2 = oxml.match[i].mscr.blgTm.@sName.toString();
+					let score1 = oxml.match[i].mscr.btTm.Inngs;
+					let score2 = oxml.match[i].mscr.blgTm.Inngs;
+					let more = oxml.match[i].mscr.inngsdetail;
+					//let link = oxml.match[i].child("url-link").@href;
+					let scoreText = null;
+					scoreText = team1  +  " - ";
+					scoreText += score1.@r.toString() + " / " ;
+					scoreText += score1.@wkts.toString() + " ( "  + score1.@ovrs.toString() + " )  vs " + team2 ;
+					this._scoreInfo.text = scoreText;
+					var hasScore= (oxml.match[i].mscr.blgTm.children().length() > 0);
+					if(!hasScore) { 
+						this._scoreInfo.text += " - " + score2.@r.toString() + " / " + score2.@wkts.toString() + " ( "  + score2.@ovrs.toString() + " ) ";
+					}       
+
+					let moreInfo = "";
+					let requiredRR = more.@rrr.toString();
+					if(requiredRR != "") { 
+					    let moreInfo = "Required RunRate   : " + requiredRR + "\n\n";
+					}
+					moreInfo  += "Current RunRate   : " + more.@crr.toString();
+					moreInfo += "\n\n" + "Current PrtnrShip : " + more.@cprtshp.toString();
+					let refreshedDate  =  "Last Refreshed on  " + currentTime;
+					this._moreInfo.get_child().text = moreInfo;
+					this._refreshedDate.get_child().text = refreshedDate;
+				}
+			break;
+		}
+
+	    }
+	    catch(e){
+		global.log('A ' + e.name + ' has occured: ' + e.message);
+	    }
+	
+	    if(recurse) {
+		this._timeoutS = Mainloop.timeout_add_seconds(3, Lang.bind(this, function() {
+		this.refreshScore(true);}));
+	    }});
+    	},
 
 
-    refreshScore: function(recurse) {
-        url = "http://synd.cricbuzz.com/j2me/1.0/livematches.xml";
-        this.load_xml_async(url , function(content) {
-            try {
-              // ECMA Script for XML (E4X)
-             let oxml  = new XML(content);
-              i=0;
-              while(typeof(oxml.match[i]) != 'undefined'){
-                        var d  = new Date();
-                        var hrs = (d.getHours() < 10) ? ("0" + d.getHours()) : d.getHours();
-                        var min = (d.getMinutes() < 10) ? ("0" + d.getMinutes()) : d.getMinutes();
-                        var sec = (d.getSeconds() < 10) ? ("0" + d.getSeconds()) : d.getSeconds();
-                        var currentTime = hrs + " : " + min + " : " + sec + " IST ";
-                        if(oxml.match[i].state.@mchState == "preview") {
-                          let team1 = oxml.match[i].Tm[0].@sName.toString();
-                          let team2 = oxml.match[i].Tm[1].@sName.toString();
-                          let status = oxml.match[i].state.@status;
-
-                          this._scoreInfo.text = team1 + " vs " + team2 + " ( Upcoming ) ";
-                          this._moreInfo.get_child().text = "Dont waste your time ! Match " + status; 
-                          this._refreshedDate.get_child().text = "  Last Refreshed on   " + currentTime; 
-                        } else { 
-                               let team1 = oxml.match[i].mscr.btTm.@sName.toString();
-                               let team2 = oxml.match[i].mscr.blgTm.@sName.toString();
-                               let score1 = oxml.match[i].mscr.btTm.Inngs;
-                               let score2 = oxml.match[i].mscr.blgTm.Inngs;
-                               let more = oxml.match[i].mscr.inngsdetail;
-                               //let link = oxml.match[i].child("url-link").@href;
-                                this._scoreInfo.text = team1  +  " - " + score1.@r.toString() + " / " + score1.@wkts.toString() + " ( "  + score1.@ovrs.toString() + " )  vs " + team2 ;
-                                var hasScore= (oxml.match[i].mscr.blgTm.children().length() > 0);
-                                if(!hasScore) { 
-                                        this._scoreInfo.text += " - " + score2.@r.toString() + " / " + score2.@wkts.toString() + " ( "  + score2.@ovrs.toString() + " ) ";
-                                 }       
-
-                                 let moreInfo = "";
-                                 let requiredRR = more.@rrr.toString();
-                                 if(requiredRR != "") { 
-                                    let moreInfo = "Required RunRate   : " + requiredRR + "\n\n";
-                                }
-                                moreInfo  += "Current RunRate   : " + more.@crr.toString();
-                                moreInfo += "\n\n" + "Current PrtnrShip : " + more.@cprtshp.toString();
-                                let refreshedDate  =  "Last Refreshed on  " + currentTime;
-                                this._moreInfo.get_child().text = moreInfo;
-                                this._refreshedDate.get_child().text = refreshedDate;
-                        }
-                  break;
-              }
-
-            }
-            catch(e){
-                global.log('A ' + e.name + ' has occured: ' + e.message);
-            }
-            if(recurse) {
-                    this._timeoutS = Mainloop.timeout_add_seconds(3, Lang.bind(this, function() {
-                     this.refreshScore(true);
-                   }));
-           }
-
-    });
-
-    },
-
-
-      showLoadingUi: function() {
-         this.destroyCurrentInfo();
-         this._moreInfo.set_child(new St.Label({ style_class:'moreInfo' , text: _('Loading More Info ...') }));
-        this._refreshedDate.set_child(new St.Label({ style_class:'moreInfo' , text: _('Loading  Last Refreshed Date...') }));
-
-    },
+	showLoadingUi: function() {
+		this.destroyCurrentInfo();
+		this._moreInfo.set_child(new St.Label({ style_class:'moreInfo' , text: _('Loading More Info ...') }));
+		this._refreshedDate.set_child(new St.Label({ style_class:'moreInfo' , text: _('Loading  Last Refreshed Date...') }));
+	},
 
 	stop : function()
 	{
@@ -198,11 +196,11 @@ function init() {
 }
 
 function enable() {
-    cricketScore = new cricketScoreButton();
-    Main.panel.addToStatusArea('cricketMenu', cricketScore);
+	cricketScore = new cricketScoreButton();
+	Main.panel.addToStatusArea('cricketMenu', cricketScore);
 }
 
 function disable() {
-  cricketScore.stop();
-  cricketScore.destroy();
+	cricketScore.stop();
+	cricketScore.destroy();
 }
